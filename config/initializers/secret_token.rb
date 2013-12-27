6e71883f91386597a788bc5e264ed1b94ba11f21
Nancy <nancy.richardson@onthebeach.co.uk>
2013-12-27 18:44:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '76c38557600b240504d826663f2c6ac3f491ff708d9f30f22a039b503594a2041530d7857e3bf1462c42a9d5c49407bebf887324a08dfe5889143f50b9025a26'
